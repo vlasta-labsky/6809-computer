3db3740bc1c59e749af7f14a0d20a765c3e41cc0
vlastik <labsky@kv2.cz>
2021-06-20 19:15:37 +0200

    Improve test program

--- sw/test1/test1.asm
+++ sw/test1/test1.asm
@@ -16,8 +16,55 @@ PUTCHAR_WAIT
 	stb	TX
 	puls	A, PC	
 
+DELAY_50MS
+	pshs	A, B
+	ldb	#$0
+
+DELAY_50MS_LOOP1
+	lda	#$0
+DELAY_50MS_LOOP2
+	inca
+	cmpa	#$ff
+	bne	DELAY_50MS_LOOP2
+	
+	incb
+	cmpb	#$24
+	bne	DELAY_50MS_LOOP1
+
+	puls	A, B, PC
+
+
+DELAY_1S
+	pshs	A
+	lda	#$0
+DELAY_1S_LOOP
+	inca
+
+	jsr	DELAY_50MS
+
+	cmpa	#$14
+	bne	DELAY_1S_LOOP
+	
+	puls	A, PC
+
+
 RESVEC
 	lds	#$1000	; stack top
+
+	jsr	DELAY_1S
+
+	ldb	#$1b    ; reset VFD display
+	jsr	PUTCHAR
+	ldb	#$49
+	jsr	PUTCHAR
+	
+	jsr	DELAY_1S
+
+	ldb	#$0e    ; clear screen
+	jsr	PUTCHAR
+	ldb	#$0C
+	jsr	PUTCHAR
+
 LOOP1
 	lda	#$40	
 LOOP2
@@ -27,6 +74,8 @@ LOOP2
 	tfr	A, B
 	jsr	PUTCHAR	
 
+	jsr	DELAY_50MS
+
 	;jmp LOOP
 	cmpa	#$50
 	bne	LOOP2
@@ -35,6 +84,12 @@ LOOP2
 	;stb	TX
 
 	jsr	PUTCHAR
+
+	ldb	#$d
+	jsr	PUTCHAR
+
+
+	jsr	DELAY_1S
 
 	jmp	LOOP1
 	
