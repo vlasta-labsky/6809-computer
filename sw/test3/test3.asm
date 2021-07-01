9588b3b42614409974b032a1394e6a9ba7e29225
vlastik <labsky@kv2.cz>
2021-07-01 16:51:41 +0200

    Improve test program

--- sw/test3/test3.asm
+++ sw/test3/test3.asm
@@ -15,6 +15,37 @@ PUTCHAR_WAIT
 
 	stb	TX
 	puls	A, PC	
+	
+DELAY_50MS
+	pshs	A, B
+	ldb	#$0
+
+DELAY_50MS_LOOP1
+	lda	#$0
+DELAY_50MS_LOOP2
+	inca
+	cmpa	#$ff
+	bne	DELAY_50MS_LOOP2
+	
+	incb
+	cmpb	#$24
+	bne	DELAY_50MS_LOOP1
+
+	puls	A, B, PC
+
+
+DELAY_1S
+	pshs	A
+	lda	#$0
+DELAY_1S_LOOP
+	inca
+
+	jsr	DELAY_50MS
+
+	cmpa	#$14
+	bne	DELAY_1S_LOOP
+	
+	puls	A, PC
 
 PRINT_NUM
 	pshs	A, B
@@ -82,17 +113,36 @@ PRINT_NUM_1
 
 RESVEC
 	lds	#$1000	; stack top
+	
+	jsr	DELAY_1S
+
+	ldb	#$1b    ; reset VFD display
+	jsr	PUTCHAR
+	ldb	#$49
+	jsr	PUTCHAR
+	
+    	jsr	DELAY_1S
+
+    	ldb	#$0e    ; clear screen
+	jsr	PUTCHAR
+	ldb	#$0C
+	jsr	PUTCHAR
 
 	lda	#0
 LOOP1
 	tfr	A, B
 	jsr	PRINT_NUM		
 
+	ldb	#$d
+	jsr	PUTCHAR
+	
 	ldb	#$a
 	jsr	PUTCHAR
 
 	inca
 
+	jsr	DELAY_1S
+	
 	jmp	LOOP1
 	
 ; interrupt vectors
